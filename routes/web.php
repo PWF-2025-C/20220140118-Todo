577ec051ee96f41542aed500a6c29d2774f6cf9a
emzhran <zachran750@gmail.com>
2025-05-06 16:03:48 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
     // Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
     Route::patch('todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
-    Route::patch('todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::patch('todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
     Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
     Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+});
 
+Route::middleware(['auth', 'admin'])->group(function(){
+    Route::resource('user', UserController::class)->except(['show']);
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
     Route::patch('/user/{user}/removedadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
---
+++ tugas7.md
@@ -0,0 +1,7 @@
+# tugas 7
+
+## 1. Middleware & Gate Not Actived
+![Alt Text](screenshot/tugas7/middleware&gate-not-actived.png)
+
+## 2. Middleware & Gate Actived
+![Alt Text](screenshot/tugas7/middleware&gate-actived.png)
